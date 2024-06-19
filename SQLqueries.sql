71b3832cbee90b7d28a1a1dc36c2c96126c92030
Jack Baylor <jackc.baylor@gmail.com>
2024-06-19 11:57:15 -0400

    Sql queries

---
+++ SQLqueries.sql
@@ -0,0 +1,13 @@
+﻿/*Customer name for OrderID 10310*/
+SELECT Orders.OrderID, Orders.CustomerID, Customers.CustomerID, Customers.CustomerName
+FROM Orders
+JOIN Customers 
+ON Orders.CustomerID=Customers.CustomerID
+WHERE Orders.OrderID = 10310;
+
+/*address for the supplier of ProductID 40*/
+SELECT Products.ProductID, Products.SupplierID, Suppliers.SupplierID, Suppliers.Address 
+FROM Products
+JOIN Suppliers
+ON Products.SupplierID = Suppliers.SupplierID
+WHERE Products.ProductID = 40;
